fd325ec33ef48852e0cc1b1ee256fe4411f253d1
김성현 <kimsh4265@korea.ac.kr>
2021-08-24 20:13:54 +0900

    change prod allow hosts

--- clockproject/settings/prod.py
+++ clockproject/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['3.38.3.105']
+ALLOWED_HOSTS = ['3.38.3.105', 'endingclock.tk/', 'www.endingclock.tk/']
 STATIC_ROOT = BASE_DIR / 'static/'
