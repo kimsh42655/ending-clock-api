b54093211a5fd1e5376ac7a9eacc9ce2347a99af
김성현 <kimsh4265@korea.ac.kr>
2021-08-25 00:50:35 +0900

    change url

--- clockproject/settings/prod.py
+++ clockproject/settings/prod.py
@@ -2,5 +2,5 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.38.3.105', 'endingclock.tk', 'www.endingclock.tk']
+ALLOWED_HOSTS = ['3.38.3.105', 'endingclock.xyz']
 STATIC_ROOT = BASE_DIR / 'static/'
